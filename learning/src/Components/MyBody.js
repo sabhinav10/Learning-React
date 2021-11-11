8d184dab4947c5e1901979031ec01619dc68cd3f
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 21:58:28 +0530

    body component

---
+++ learning/src/Components/MyBody.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MyBody(){
+    return(
+        <body>
+        <p>I'm learning React and it's going awesome</p>
+        <ul>The palces I want to vist:
+            <li>Paris</li>
+            <li>Amsteradam</li>
+            <li>Dehradun</li>
+        </ul>
+        </body>
+    )
+}
+export default MyBody;
