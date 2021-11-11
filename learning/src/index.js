693ac70e2ed4cbf695964fb83e60ea5745f4323a
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 21:11:06 +0530

    importing MyInfo from Components and rendering

--- learning/src/index.js
+++ learning/src/index.js
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MyInfo from "./Components/MyInfo"
 //Creating a MyInfo functional component and rendering it
 //You need to wrap all elements in a single div or any other html element ,because only one JSX element can be returned and rendered.
 
-function MyInfo(){
-  return(
-    <div>
-      <h1>Hi!! My name is Abhinav😀</h1>
-      <p>I'm learning React and it's going awesome</p>
-      <ul>The palces I want to vist:
-        <li>Paris</li>
-        <li>Amsteradam</li>
-        <li>Dehradun</li>
-      </ul>
-    </div>
-  )
-}
 ReactDOM.render(<MyInfo/>,document.getElementById("root"));
