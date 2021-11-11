572cafdf08bfbdfd9b269cf709da4094c9305337
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 21:10:27 +0530

    setting Components folder and creating MyInfo

---
+++ learning/src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo(){
+  return(
+    <div>
+      <h1>Hi!! My name is Abhinav😀</h1>
+      <p>I'm learning React and it's going awesome</p>
+      <ul>The palces I want to vist:
+        <li>Paris</li>
+        <li>Amsteradam</li>
+        <li>Dehradun</li>
+      </ul>
+    </div>
+  )
+};
+//exporting JSX using MyInfo functional component
+export default MyInfo;
