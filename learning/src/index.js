608eccdf83371d39f35ce5f2912f79a7bfcdfe8a
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 20:32:07 +0530

    Practice

--- learning/src/index.js
+++ learning/src/index.js
@@ -3,4 +3,5 @@ import React from "react";
 //ReactDOM is imported as it contains the render function 
 import ReactDOM from "react-dom";
 //ReactDOM.render(what you want to render , where you want to render)
-ReactDOM.render(<h1>Getting strted with React</h1>,document.getElementById("root"));
+ReactDOM.render(<ul><li>Ironman</li><li>Batman</li><li>Superman</li></ul>,document.getElementById("root"));
+
