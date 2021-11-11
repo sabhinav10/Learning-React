d64a783043cdabe55400c6bb624b65b44e9e35b8
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 21:59:12 +0530

    Parent component

--- learning/src/Components/MyInfo.js
+++ learning/src/Components/MyInfo.js
@@ -1,15 +1,14 @@
 import React from "react"
+import MyHeader from "./MyHeader";
+import MyBody from "./MyBody";
+import MyFooter from "./MyHeader";
 
 function MyInfo(){
   return(
     <div>
-      <h1>Hi!! My name is Abhinav😀</h1>
-      <p>I'm learning React and it's going awesome</p>
-      <ul>The palces I want to vist:
-        <li>Paris</li>
-        <li>Amsteradam</li>
-        <li>Dehradun</li>
-      </ul>
+      <MyHeader/>
+      <MyBody/>
+      <MyFooter/>
     </div>
   )
 };
