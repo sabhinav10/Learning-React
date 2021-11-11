0a8f6a99580a41895d3f6422f34e47b27e01d0e6
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 21:58:12 +0530

    header component

---
+++ learning/src/Components/MyHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MyHeader(){
+    return(
+        <header>
+            <h1>Hi!! My name is Abhinav😀</h1>
+        </header>
+    )
+}
+export default MyHeader;
