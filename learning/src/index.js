53252ee377900ac82825f84b4d725edf97f6fff4
sabhinav10 <sabhinav654@gmail.com>
2021-11-11 20:57:21 +0530

    practice functional components

--- learning/src/index.js
+++ learning/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//Creating functional components,always follow camel case convention used for constructors(first letter of each word is capital)
-//Put the ul in the return statement with brackets enclosed to make it more readable
-function MyApp(){
-  return(
-  <ul>
-    <li>Ironman</li>
-    <li>Batman</li>
-    <li>Superman</li>
-  </ul>
-  );
-}
-//To render it ,create an instance of the functional component as this:<MyApp/>
-ReactDOM.render(<MyApp/>,document.getElementById("root"));
+//Creating a MyInfo functional component and rendering it
+//You need to wrap all elements in a single div or any other html element ,because only one JSX element can be returned and rendered.
 
+function MyInfo(){
+  return(
+    <div>
+      <h1>Hi!! My name is Abhinav😀</h1>
+      <p>I'm learning React and it's going awesome</p>
+      <ul>The palces I want to vist:
+        <li>Paris</li>
+        <li>Amsteradam</li>
+        <li>Dehradun</li>
+      </ul>
+    </div>
+  )
+}
+ReactDOM.render(<MyInfo/>,document.getElementById("root"));
