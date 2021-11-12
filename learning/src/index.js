6461def45f4a04390a22b183fb50218d7f692438
sabhinav10 <sabhinav654@gmail.com>
2021-11-12 19:35:25 +0530

    Greetings import

--- learning/src/index.js
+++ learning/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MyInfo from "./Components/MyInfo"
+import Greetings from "./Components/Greetings"
 //Creating a MyInfo functional component and rendering it
 //You need to wrap all elements in a single div or any other html element ,because only one JSX element can be returned and rendered.
 
-ReactDOM.render(<MyInfo/>,document.getElementById("root"));
+ReactDOM.render(<Greetings/>,document.getElementById("root"));
