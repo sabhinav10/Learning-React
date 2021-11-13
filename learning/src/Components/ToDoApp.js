28831c552877cac269a126c4477f31ff2e663204
sabhinav10 <sabhinav654@gmail.com>
2021-11-13 12:57:37 +0530

    ToDoApp created

--- learning/src/Components/ToDoApp.js
+++ learning/src/Components/ToDoApp.js
@@ -1,16 +1,13 @@
-import React from "react"
+import React from "react";
 import ToDoItem from "./ToDoItem";
-import "./"
-
+import "./styles.css"
 function ToDoApp(){
-  return(
-    <div className = "todo-list">
-      <ToDoItem/>
-      <ToDoItem/>
-      <ToDoItem/>
-      <ToDoItem/>
-    </div>
-  )
-};
-//exporting JSX using MyInfo functional component
+    return(
+        <div className = "todo-list">
+            <ToDoItem/>
+            <ToDoItem/>
+            <ToDoItem/>
+        </div>
+    )
+}
 export default ToDoApp;
