844f22b897287349cd54f1621303e7133ae875e3
sabhinav10 <sabhinav654@gmail.com>
2021-11-13 10:15:36 +0530

    rendering TodoApp

--- learning/src/index.js
+++ learning/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Greetings from "./Components/Greetings"
+import ToDoApp from "./Components/ToDoApp"
 //Creating a MyInfo functional component and rendering it
 //You need to wrap all elements in a single div or any other html element ,because only one JSX element can be returned and rendered.
 
-ReactDOM.render(<Greetings/>,document.getElementById("root"));
+ReactDOM.render(<ToDoApp/>,document.getElementById("root"));
