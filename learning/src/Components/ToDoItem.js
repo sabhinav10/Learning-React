f3016d9c75a4e6cd00abfc5cccce3eadc56f5757
sabhinav10 <sabhinav654@gmail.com>
2021-11-13 10:16:10 +0530

    ToDoItem Created

---
+++ learning/src/Components/ToDoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "styles.css"
+function ToDoItem(){
+    return(
+    <div className = "todo-item">
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+    </div>
+    )
+}
+export default ToDoItem;
