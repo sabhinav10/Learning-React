deac1819370cdb70045eeffa2afe08054edaa571
sabhinav10 <sabhinav654@gmail.com>
2021-12-30 21:58:44 +0530

    remvoved boilerplate and old files

--- learning/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ToDoApp from "./Components/ToDoApp"
-//Creating a MyInfo functional component and rendering it
-//You need to wrap all elements in a single div or any other html element ,because only one JSX element can be returned and rendered.
-
-ReactDOM.render(<ToDoApp/>,document.getElementById("root"));
